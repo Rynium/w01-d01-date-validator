fb40c952a321fc779e4ae2ad8446aa49ac6c2fab
Ryan Stantz <ryanstantz@gmail.com>
2016-03-15 16:17:40 -0500

    methods separated

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,28 +1,41 @@
-def valid_date?(month, day, year)
+require 'pry'
 
-    #validates the year is 1880 to 2280 inc and month 1 to 12 inc.
-    return false if  (year < 1880 || year > 2280)  ||  (month < 1 || month > 12)
 
-    #validates the day for each month.
-    case month
-
-        #validates day for 31 day months.
-        when 1,3,5,7,8,10,12
-            return false if !(day < 32 && day > 0)
-
-        #validates day for 30 day months.
-        when 4,6,9,11
-            return false if !(day < 31 && day > 0)
-
-        #validates day for FEB
-        when 2
-          if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-            return false if !(day > 0 && day < 30)
-
-          else
-            return false if !(day > 0 && day < 29)
-
-        end
-  end
-  return true
+#validates the year is 1880 to 2280
+#takes year as a parameter that is a 4-digit integer
+#true is returned if the year parameter is between 1880 and 2280 inclusive.
+def year_valid?(year)
+  return false if year < 1880 || year > 2280
 end
+binding.pry
+
+#validates the month is between 1 and 12
+#takes the integer month as a parameter
+#true is returned if the month integer parameter is between 1 and 12 inclusive.
+def month_valid?(month)
+  return false if month < 1 || month > 12
+end
+binding.pry
+
+#method takes in year and determines if it is a leap year.
+#takes the integer year as a parameter
+#true is returned if the year is a leap year.
+def leap_year?(month)
+  return false if !(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+end
+binding.pry
+
+#takes month and retruns validator_31_day_month, validator_30_day_month, or validator_febrary
+#takes month integer as a parameter
+#true is returned if the year parameter is between 1880 and 2280 inclusive.
+def month_route(month)
+  case month
+        when 1,3,5,7,8,10,12
+            return validator_31_day_month
+        when 4,6,9,11
+            return validator_30_day_month
+        when 2
+            return validator_febrary
+  end
+end
+binding.pry
