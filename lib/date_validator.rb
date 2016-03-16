deec3a2d3af99d926fa75897a9ab4f74b5a40fd7
Ryan Stantz <ryanstantz@gmail.com>
2016-03-15 19:11:49 -0500

    Complete Pre-Test

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,41 +1,51 @@
 require 'pry'
 
+out = ''
+tru = true
 
-#validates the year is 1880 to 2280
-#takes year as a parameter that is a 4-digit integer
-#true is returned if the year parameter is between 1880 and 2280 inclusive.
 def year_valid?(year)
-  return false if year < 1880 || year > 2280
+  tru = false if year < 1880 || year > 2280
+  tru
 end
-binding.pry
 
-#validates the month is between 1 and 12
-#takes the integer month as a parameter
-#true is returned if the month integer parameter is between 1 and 12 inclusive.
 def month_valid?(month)
-  return false if month < 1 || month > 12
+  tru = true
+  out = false if month < 1 || month > 12
+  out
+end
+
+def leap_year?(year)
+  tru = false if !(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+  tru
+end
+
+def day_validating_month_router(month, day, year)
+  out = validator_31(day) if [1,3,5,7,8,10,12].include? month
+  out = validator_30(day) if [4,6,9,11].include? month
+  out = validator_29(day) if month = 2 && leap_year?(year)
+  out = validator_28(day) if month = 2 && !leap_year?(year)
+  out
+end
+
+def validator_31(day)
+  !(day < 32 && day > 0)? false : true
+end
+
+def validator_30(day)
+  !(day < 31 && day > 0)? false : true
+end
+
+def validator_29(day)
+  !(day < 30 && day > 0)? false : true
+end
+
+def validator_28(day)
+  !(day < 29 && day > 0)? false : true
 end
 binding.pry
 
-#method takes in year and determines if it is a leap year.
-#takes the integer year as a parameter
-#true is returned if the year is a leap year.
-def leap_year?(month)
-  return false if !(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+def date_validator(month,day,year)
+  year_valid?(year)
+  month_valid?(month)
+  day_validating_month_router(month, day, year)
 end
-binding.pry
-
-#takes month and retruns validator_31_day_month, validator_30_day_month, or validator_febrary
-#takes month integer as a parameter
-#true is returned if the year parameter is between 1880 and 2280 inclusive.
-def month_route(month)
-  case month
-        when 1,3,5,7,8,10,12
-            return validator_31_day_month
-        when 4,6,9,11
-            return validator_30_day_month
-        when 2
-            return validator_febrary
-  end
-end
-binding.pry
