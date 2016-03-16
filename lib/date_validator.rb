fa5900d3e48a51f7ad0bcaa661343550273a5ab3
Ryan Stantz <ryanstantz@gmail.com>
2016-03-15 20:40:24 -0500

    Done with blocking of date validation code

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,51 +1,32 @@
-require 'pry'
-
-out = ''
-tru = true
-
-def year_valid?(year)
-  tru = false if year < 1880 || year > 2280
-  tru
-end
-
-def month_valid?(month)
-  tru = true
+def month_day_year_valid?(month, day, year)
+  out = true
+  out = false if year < 1880 || year > 2280
+  out = false if day < 1
   out = false if month < 1 || month > 12
   out
 end
 
 def leap_year?(year)
-  tru = false if !(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-  tru
-end
-
-def day_validating_month_router(month, day, year)
-  out = validator_31(day) if [1,3,5,7,8,10,12].include? month
-  out = validator_30(day) if [4,6,9,11].include? month
-  out = validator_29(day) if month = 2 && leap_year?(year)
-  out = validator_28(day) if month = 2 && !leap_year?(year)
+  out = false if !(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
   out
 end
 
-def validator_31(day)
-  !(day < 32 && day > 0)? false : true
+def day_validating_month_router(month, day, year)
+  out = day_validator(day,32) if [1,3,5,7,8,10,12].include? month
+  out = day_validator(day,31) if [4,6,9,11].include? month
+  out = day_validator(day,30) if month = 2 && leap_year?(year) == true
+  out = day_validator(day,29) if month = 2 && leap_year?(year) == false
+  out
 end
 
-def validator_30(day)
-  !(day < 31 && day > 0)? false : true
+def day_validator(day,limit)
+  !(day < limit)? false : true
 end
 
-def validator_29(day)
-  !(day < 30 && day > 0)? false : true
-end
 
-def validator_28(day)
-  !(day < 29 && day > 0)? false : true
-end
-binding.pry
-
-def date_validator(month,day,year)
-  year_valid?(year)
-  month_valid?(month)
-  day_validating_month_router(month, day, year)
+def valid_date?(month,day,year)
+  out = true
+  out = false if month_day_year_valid?(month, day, year) == false
+  out = false if day_validating_month_router(month, day, year) == false
+  out
 end
